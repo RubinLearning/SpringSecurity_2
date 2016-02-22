13cb31b8605f44cb78c2796f2c633049145737af
RubinatrixHome <rubinwork@gmail.com>
2016-02-22 02:28:52 +0200

    Commit 1

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -13,16 +15,33 @@ import java.util.logging.Logger;
 public class HomeController {
     private static final Logger logger = Logger.getLogger("HomeController");
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(Model model){
-        logger.info("Welcome home!");
-
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
-
-        String formattedDate = dateFormat.format(date);
-        model.addAttribute("serverTime", formattedDate);
-
-        return "home";
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String mainPage() {
+//        Date date = new Date();
+//        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+//        String formattedDate = dateFormat.format(date);
+//        model.addAttribute("serverTime", formattedDate);
+        return "/content/user";
     }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage() {
+//        Date date = new Date();
+//        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+//        String formattedDate = dateFormat.format(date);
+//        model.addAttribute("serverTime", formattedDate);
+        return "/content/admin";
+    }
+
+    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username or password!");
+        }
+
+        model.setViewName("login");
+        return model;
+    }
+
 }
--- src/main/webapp/WEB-INF/view/home.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Spring security - Home page</h2>
-<p>${serverTime}</p>
-</body>
-</html>
